5dd19a1337d780f868bc740a66c81c0962481218
Sergei <radchenkovsa@yandex.ru>
2023-08-29 11:15:51 +0300

    Case1

---
+++ case1/Program.cs
@@ -0,0 +1,42 @@
+﻿int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+/* string GetQuater(int axisX, int axisY)
+{
+    string answer = "";
+    if (axisX > 0 && axisY > 0)
+        answer = "1 четверть";
+    else if (axisX < 0 && axisY > 0)
+        answer = "2 четверть";
+    else if (axisX < 0 && axisY < 0)
+        answer = "3 четверть";
+    else if (axisX > 0 && axisY < 0)
+        answer = "4 четверть";
+    else 
+        answer = "Точка находится на границе четверти";
+
+    return answer;
+} */
+
+void GetQuater(int axisX, int axisY)
+{
+    if (axisX > 0 && axisY > 0)
+        Console.WriteLine("1 четверть");
+    else if (axisX < 0 && axisY > 0)
+        Console.WriteLine("2 четверть");
+    else if (axisX < 0 && axisY < 0)
+        Console.WriteLine("3 четверть");
+    else if (axisX > 0 && axisY < 0)
+        Console.WriteLine("4 четверть");
+    else 
+        Console.WriteLine("Точка находится на границе четверти");
+}
+
+int coorX = InputNum("Введите значение координаты X: ");
+int coorY = InputNum("Введите значение координаты Y: ");
+/* string result = GetQuater(coorX, coorY);
+Console.WriteLine(result); */
+GetQuater(coorX, coorY);
