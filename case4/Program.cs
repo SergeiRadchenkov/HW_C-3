b44444d5e3a075d9f40102a98d35638d751c30bf
Sergei <radchenkovsa@yandex.ru>
2023-08-29 13:27:07 +0300

    Case4

---
+++ case4/Program.cs
@@ -0,0 +1,17 @@
+﻿int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Squares(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int res = (int)Math.Pow(i, 2);
+        Console.WriteLine(res);
+    }
+}
+
+int number = InputNum("Введите число: ");
+Squares(number);
